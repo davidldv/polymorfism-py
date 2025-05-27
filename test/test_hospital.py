70d02f0898422757bc510b3fc058f8b58d26d1f4
David London <dlondon.dev@gmail.com>
2025-05-26 20:35:42 -0500

    some changes

--- test/test_hospital.py
+++
@@ -1,8 +0,0 @@
-from controller.hospital_controller import HospitalController
-from model.hospital import Hospital
-
-def test_creacion():
-    c = HospitalController()
-    h = Hospital("San José", "Calle 10 #5-20")
-    c.crear(h)
-    assert c.obtener_por_id("San José") == h
